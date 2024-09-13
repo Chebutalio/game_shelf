59a87b68715bfebaeb6b45335fd6d82a32fcc2e8
chebutalio <chebutalio@gmail.com>
2024-09-13 13:02:11 +0300

    added translations and tailwind, added first test words for translations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,23 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TranslateModule, TranslateService } from "@ngx-translate/core";
+import { TranslationService} from "./services/translation.service";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TranslateModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'GameShelf';
+
+  constructor(private translate: TranslateService,
+              private translationService: TranslationService) {
+    translate.setDefaultLang('en');
+  }
+
+  public changeLang(lang: string): void {
+    this.translationService.changeLang(lang);
+  }
 }
